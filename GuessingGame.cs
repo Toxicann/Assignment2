3831522b84a5645f8c9f7859381ea046a9decd97
Toxicann <you@example.com>
2020-09-15 17:45:41 +0545

    game bug fixes

--- GuessingGame.cs
+++ GuessingGame.cs
@@ -4,30 +4,52 @@ namespace Assignment2
 {
     public class GuessingGame
     {
-        public void GuessingG(){
-            Console.WriteLine("Figure Out What Number I Guessed From 1 - 100");
-            Console.WriteLine("You get 5 Tries! Choose Wisely: \t");
+        public void GuessingG()
+        {
+            string ifcont;
+            do
+            {
+                Console.WriteLine("Figure Out What Number I Guessed From 1 - 100");
+                Console.WriteLine("You get 5 Tries! Choose Wisely");
 
-            int myguess =  new Random().Next(1 ,101);
-            int count = 5;
-            int guessednumber;
+                int myguess = new Random().Next(1, 101);
+                int count = 5;
+                int guessednumber;
+                
+                // Console.WriteLine("my guess {0}", myguess);
 
-            do{
-                Console.Write("Guess a number");
-                guessednumber = Convert.ToInt32(Console.ReadLine());
+                do
+                {
+                    Console.Write("Guess a number: \t");
+                    guessednumber = Convert.ToInt32(Console.ReadLine());
 
-                if(guessednumber == myguess){
-                    Console.WriteLine("Looks like u won! GG");
-                }
-                else if(guessednumber < myguess){
-                    Console.WriteLine("Guess Something Higher , ya Noob");
-                }
-                else{
-                    Console.WriteLine("Ya went too high kiddo");
-                }
+                    if (guessednumber == myguess)
+                    {
+                        Console.WriteLine("Looks like u won! GG");
+                        break;
+                    }
+                    else if (guessednumber < myguess)
+                    {
+                        Console.WriteLine("Guess Something Higher , ya Noob");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ya went too high kiddo");
+                    }
 
-                count--;
-            }while(count != 0);
+                    count--;
+                    if (count == 0)
+                    {
+                        Console.WriteLine("Welp out of tries \nGo home now kiddo, run Along");
+                    }
+                } while (count != 0);
+
+                Console.WriteLine("Continue Gaming (Y) / Or are u a sore Loser");
+                ifcont = Console.ReadLine();
+                ifcont = ifcont.ToUpper();
+
+            } while (ifcont == "Y");
+
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,9 +5,47 @@ namespace Assignment2
     class Program
     {
         static void Main(string[] args)
-        {   
+        {
             GuessingGame guessingGame = new GuessingGame();
-            guessingGame.GuessingG();
+
+            string ifcont;
+
+            do
+            {
+                Console.WriteLine("Choose:\n 1. Multiplication Table\n 2. Mean & Median\n 3. Geometric Average\n 4. Sum & Diff betn Matrices\n 5. Guess a number and win nothing");
+                int switch_on = Int16.Parse(Console.ReadLine());
+                switch (switch_on)
+                {
+                    case 1:
+                        MulTable();
+                        break;
+
+                    case 2:
+                        MeanMedian();
+                        break;
+
+                    case 3:
+                        GeoAvg();
+                        break;
+
+                    case 4:
+                        MatricesArithmetic();
+                        break;
+
+                    case 5:
+                        guessingGame.GuessingG();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid argument");
+                        break;
+                }
+                Console.WriteLine("Continue..? Y..?");
+                ifcont = Console.ReadLine();
+                ifcont = ifcont.ToUpper();
+
+            } while (ifcont == "Y");
+
         }
 
         static void MulTable()
@@ -21,9 +59,10 @@ namespace Assignment2
             }
         }
 
-        static void MeanMedian(){
+        static void MeanMedian()
+        {
             RNumGen rNumGen = new RNumGen();
-            int [] arr = rNumGen.RandomNumGen();
+            int[] arr = rNumGen.RandomNumGen();
             int sum = 0;
             int length = arr.Length;
 
@@ -37,24 +76,27 @@ namespace Assignment2
             Array.Sort(arr);
 
             Console.WriteLine("---------------------------------");
-            Console.WriteLine("Mean: {0}",mean);
-            
-            if(length % 2 != 0){
+            Console.WriteLine("Mean: {0}", mean);
+
+            if (length % 2 != 0)
+            {
                 int medianplaceholder = Convert.ToInt32((length + 1) / 2);
                 Console.WriteLine("Median: {0}", arr[medianplaceholder]);
             }
-            else{
+            else
+            {
                 int medianplaceholder = Convert.ToInt32((length + 1) / 2);
                 Console.WriteLine("Median: {0}", medianplaceholder);
                 double median = (arr[medianplaceholder - 1] + arr[medianplaceholder]) / 2.0;
                 Console.WriteLine("Median: {0}", median);
             }
-            
+
         }
 
-        static void GeoAvg(){
+        static void GeoAvg()
+        {
             RNumGen rNumGen = new RNumGen();
-            int [] arr = rNumGen.RandomNumGen();
+            int[] arr = rNumGen.RandomNumGen();
             double mul = 1;
 
             for (int i = 0; i < arr.Length; i++)
@@ -62,23 +104,24 @@ namespace Assignment2
                 mul *= arr[i];
             }
 
-           Console.WriteLine("GeometricAverage: {0}",Math.Pow(mul , 1.0/arr.Length));
+            Console.WriteLine("GeometricAverage: {0}", Math.Pow(mul, 1.0 / arr.Length));
         }
 
-        static void MatricesArithmetic(){
+        static void MatricesArithmetic()
+        {
             Console.Write("Enter no. of Rows");
             int r = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter no. of Columns");
             int c = Convert.ToInt32(Console.ReadLine());
 
-            int [,] array1 = new int[r,c], array2 = new int[r,c], sum = new int[r,c], diff = new int[r,c];
+            int[,] array1 = new int[r, c], array2 = new int[r, c], sum = new int[r, c], diff = new int[r, c];
 
             Console.WriteLine("Enter Values in Array1");
             for (int i = 0; i < r; i++)
             {
                 for (int j = 0; j < c; j++)
                 {
-                   array1[i,j] = Convert.ToInt32(Console.ReadLine());
+                    array1[i, j] = Convert.ToInt32(Console.ReadLine());
                 }
             }
 
@@ -87,7 +130,7 @@ namespace Assignment2
             {
                 for (int j = 0; j < c; j++)
                 {
-                   array2[i,j] = Convert.ToInt32(Console.ReadLine());
+                    array2[i, j] = Convert.ToInt32(Console.ReadLine());
                 }
             }
 
@@ -96,8 +139,8 @@ namespace Assignment2
             {
                 for (int j = 0; j < c; j++)
                 {
-                   sum[i,j]  = array1[i,j] + array2[i,j];
-                   diff[i,j] = array1[i,j] - array2[i,j];
+                    sum[i, j] = array1[i, j] + array2[i, j];
+                    diff[i, j] = array1[i, j] - array2[i, j];
                 }
             }
 
@@ -108,8 +151,8 @@ namespace Assignment2
             {
                 for (int j = 0; j < c; j++)
                 {
-                   Console.Write(sum[i ,j]);
-                   Console.Write("\t");
+                    Console.Write(sum[i, j]);
+                    Console.Write("\t");
                 }
                 Console.WriteLine("");
             }
@@ -121,8 +164,8 @@ namespace Assignment2
             {
                 for (int j = 0; j < c; j++)
                 {
-                   Console.Write(diff[i ,j]);
-                   Console.Write("\t");
+                    Console.Write(diff[i, j]);
+                    Console.Write("\t");
                 }
                 Console.WriteLine("");
             }
